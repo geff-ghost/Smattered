ea35c563de2f7190bc538394681b11522444b943
Geff <geoffrey.ochieng.odhiambo03@gmail.com>
2025-11-11 06:11:20 +0300

    Alert Dialog Screen

---
+++ lib/app_screens/alertdialog_screen.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class FlightBookButton extends StatelessWidget {
+  const FlightBookButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {
+        bookFlight(context);
+      },
+      child: Text(
+        'Book Your Flight',
+        style: TextStyle(
+          fontSize: 20.0,
+          color: Colors.deepOrange,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  void bookFlight(BuildContext context) {
+    var alertDialog = AlertDialog(
+      title: Text('Flight Booked Successfully'),
+      content: Text('Have a pleasant flight'),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.refresh),
+          iconSize: 50.0,
+          color: Colors.amber,
+        ),
+      ],
+    );
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alertDialog;
+      },
+    );
+  }
+}
--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+import 'package:smattered/app_screens/alertdialog_screen.dart';
+
 class FirstScreen extends StatelessWidget {
   const FirstScreen({super.key});
 
@@ -9,13 +11,28 @@ class FirstScreen extends StatelessWidget {
     return Material(
       color: Colors.blueAccent,
       child: Center(
-        child: Text(
-          generateLuckyNumber(),
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 24.0,
-            color: Colors.white,
-          ),
+        child: Column(
+          children: [
+            Text(
+              generateLuckyNumber(),
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 24.0,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 15.0),
+            Text(
+              'Below this text should be an Airplane image',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 15.0),
+            FlightBookButton(),
+          ],
         ),
       ),
     );
