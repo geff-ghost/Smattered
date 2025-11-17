6e06fc33c2da25bca585b0fbfe01c7267bfa6b7a
Geff <geoffrey.ochieng.odhiambo03@gmail.com>
2025-11-17 05:41:59 +0300

    Added Image and Font assets

--- lib/app_screens/first_screen.dart
+++ lib/app_screens/first_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
-
 import 'package:smattered/app_screens/alertdialog_screen.dart';
 
 class FirstScreen extends StatelessWidget {
@@ -9,27 +8,22 @@ class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.blueAccent,
+      color: Colors.deepPurple,
       child: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Text(
               generateLuckyNumber(),
               style: TextStyle(
-                fontWeight: FontWeight.bold,
+                fontFamily: 'Raleway',
+                fontWeight: FontWeight.w700,
                 fontSize: 24.0,
                 color: Colors.white,
               ),
             ),
             SizedBox(height: 15.0),
-            Text(
-              'Below this text should be an Airplane image',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+            FlightImageAsset(),
             SizedBox(height: 15.0),
             FlightBookButton(),
           ],
@@ -42,5 +36,16 @@ class FirstScreen extends StatelessWidget {
     var random = Random();
     int luckyNumber = random.nextInt(10);
     return 'Your lucky number is $luckyNumber';
+  }
+}
+
+class FlightImageAsset extends StatelessWidget {
+  const FlightImageAsset({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    AssetImage assetImage = AssetImage('images/flight.png');
+    Image image = Image(image: assetImage);
+    return Container(child: image);
   }
 }
